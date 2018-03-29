42e02fc8996e3ba0954b20666a96b80b59fe79b9
Taimoor Khan <taimoor.m.k@live.com>
2018-03-29 17:26:20 +0500

    Further app optimisations

--- src/js/components/LetsBeInTouch.jsx
+++ src/js/components/LetsBeInTouch.jsx
@@ -18,11 +18,13 @@ class LetsBeInTouch extends Component {
       <section id="lets-be-in-touch">
         <div className="wrapper">
           <h3 className="lets-be-in-touch-heading">Lets be in Touch</h3>
-          <h4 className="lets-be-in-touch-subheading">Society excited by cottage private an it esteems. Fully begin on by wound an. Girl rich in do up or both. At declared in as rejoiced of together.</h4>
-          <form action="">
-            <input type="email" name="email" />
-            <input type="submit" value="Submit" />
+          <h4 className="lets-be-in-touch-subheading">Society excited by cottage private an it esteems. Fully begin on by wound an. <br />Girl rich in do up or both. At declared in as rejoiced of together.</h4>
+          <form className="lets-be-in-touch-form" action="">
+            <input className="lets-be-in-touch-input" type="text" placeholder="Name" name="name" />
+            <input className="lets-be-in-touch-input" type="email" placeholder="Email" name="email" />            
+            <input className="lets-be-in-touch-button" type="submit" value="Submit" />
           </form>
+          <div className="filler"></div>
         </div>
       </section>
     );
