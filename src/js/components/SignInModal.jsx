dc2d16b1c6f09f5513c35474c5bea5f167489eee
Taimoor Khan <taimoor.m.k@live.com>
2018-04-04 10:48:30 +0500

    fixed all lint errors

--- src/js/components/SignInModal.jsx
+++ src/js/components/SignInModal.jsx
@@ -1,43 +1,45 @@
 // ########## Import Dependencies Here ##########
-import React, { Component } from 'react';
-import { arrayOf, shape } from 'prop-types';
+import React from 'react';
+import { func } from 'prop-types';
 import AriaModal from 'react-aria-modal';
 
 // ########## Import Containers Here ##########
 
 // ########## Import Components Here ##########
 
-class SignInModal extends Component {
-  render() {
-    const { deactivateModalAction, getApplicationNodeAction } = this.props;
-    return (
-      <div id="signin-modal">
-        <AriaModal
-          titleText="Signin Modal"
-          onExit={() => deactivateModalAction()}
-          initialFocus=".signin-modal-body"
-          underlayStyle={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'coloumn' }}
-        >
-          <div className="signin-modal">
-            <div className="signin-modal-body">
-              <img className="signin-modal-left-background" src="./images/signin-modal-left-background.png" alt="Signin Modal left background" />
-              <img className="signin-modal-left-character" src="./images/signin-modal-left-character.png" alt="Signin Modal left character" />
-              <div className="signin-modal-right-heading">Login with Facebook or Google</div>
-              <div className="signin-modal-right-login">
-                <img className="facebook-login-button" src="./images/facebook-login-button.png" alt="Facebook login button" />
-                <img className="google-login-button" src="./images/google-login-button.png" alt="Google login button" />                
-              </div>
+const SignInModal = (props) => {
+  const { deactivateModalAction } = props;
+  return (
+    <div id="signin-modal">
+      <AriaModal
+        titleText="Signin Modal"
+        onExit={() => deactivateModalAction()}
+        initialFocus=".signin-modal-body"
+        underlayStyle={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'coloumn' }}
+      >
+        <div className="signin-modal">
+          <div className="signin-modal-body">
+            <img className="signin-modal-left-background" src="./images/signin-modal-left-background.png" alt="Signin Modal left background" />
+            <img className="signin-modal-left-character" src="./images/signin-modal-left-character.png" alt="Signin Modal left character" />
+            <div className="signin-modal-right-heading">Login with Facebook or Google</div>
+            <div className="signin-modal-right-login">
+              <img className="facebook-login-button" src="./images/facebook-login-button.png" alt="Facebook login button" />
+              <img className="google-login-button" src="./images/google-login-button.png" alt="Google login button" />
             </div>
-            {/* <footer className="modal-footer"> */}
-              {/* <button id="demo-one-deactivate" onClick={() => deactivateModalAction()}>
+          </div>
+          {/* <footer className="modal-footer"> */}
+          {/* <button id="demo-one-deactivate" onClick={() => deactivateModalAction()}>
                 deactivate modal
               </button> */}
-            {/* </footer> */}
-          </div>
-        </AriaModal>
-      </div>
-    );
-  }
+          {/* </footer> */}
+        </div>
+      </AriaModal>
+    </div>
+  );
+}
+
+SignInModal.propTypes = {
+  deactivateModalAction: func.isRequired,
 }
 
 export default SignInModal;
