db2b1926887f8338e7980f2aa51cb995b48eef52
Taimoor Khan <taimoor.m.k@live.com>
2018-04-30 14:27:04 +0500

    DashboardAppBar linting

--- src/js/components/Dashboard/DashboardAppBar.jsx
+++ src/js/components/Dashboard/DashboardAppBar.jsx
@@ -1,9 +1,7 @@
-import React, { Component } from 'react';
-import { withStyles } from 'material-ui/styles';
+import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
-import Buttoon from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
@@ -20,22 +18,22 @@ const styles = {
   },
 };
 
-export default class DashboardAppBar extends Component {
-  render() {
-    const { toggleVisibility } = this.props;
-    return (
-      <div className={styles.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton onClick={toggleVisibility} className={styles.menuButton} color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="title" color="inherit" className={styles.flex}>
-              Dashboard
+const DashboardAppBar = (props) => {
+  const { toggleVisibility } = props;
+  return (
+    <div className={styles.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton onClick={toggleVisibility} className={styles.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit" className={styles.flex}>
+            Dashboard
           </Typography>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
+
+export default DashboardAppBar;
