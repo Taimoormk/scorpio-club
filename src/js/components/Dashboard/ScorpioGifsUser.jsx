83420180342f8bde65069117e67cf5a602372c60
Taimoor Khan <taimoor.m.k@live.com>
2018-04-30 14:15:24 +0500

    ScorpioGifsUser linting

--- src/js/components/Dashboard/ScorpioGifsUser.jsx
+++ src/js/components/Dashboard/ScorpioGifsUser.jsx
@@ -1,46 +1,29 @@
-import React, { Component } from 'react';
-import { Card, Image, Icon, Grid } from 'semantic-ui-react';
-import {
-  FacebookShareButton,
-  GooglePlusShareButton,
-  LinkedinShareButton,
-  TwitterShareButton,
-  TelegramShareButton,
-  WhatsappShareButton,
-  PinterestShareButton,
-  VKShareButton,
-  OKShareButton,
-  RedditShareButton,
-  TumblrShareButton,
-  LivejournalShareButton,
-  EmailShareButton,
-} from 'react-share';
+import React from 'react';
+import { Card, Image, Grid } from 'semantic-ui-react';
 import SocialShare from './SocialShare';
 
-export default class ScorpioGifsUser extends Component {
-  render() {
-    return (
-      <Grid.Column computer={5} mobile={8}>
-        <Card>
-          <Image src='./images/patrick.png' />
-          <Card.Content>
-            <Card.Header>
-              Matthew
-            </Card.Header>
-            <Card.Meta>
-              <span className='date'>
-                November, 16<sup>th</sup>
-              </span>
-            </Card.Meta>
-            <Card.Description>
-              Matthew is a musician living in Nashville. Matthew is a musician living in Nashville. Matthew is a musician living in Nashville.
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
-            <SocialShare />
-          </Card.Content>
-        </Card>
-      </Grid.Column>
-    );
-  }
-}
+const ScorpioGifsUser = () => (
+  <Grid.Column computer={5} mobile={8}>
+    <Card>
+      <Image src='./images/patrick.png' />
+      <Card.Content>
+        <Card.Header>
+          Matthew
+        </Card.Header>
+        <Card.Meta>
+          <span className='date'>
+            November, 16<sup>th</sup>
+          </span>
+        </Card.Meta>
+        <Card.Description>
+          Matthew is a musician living in Nashville. Matthew is a musician living in Nashville. Matthew is a musician living in Nashville.
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <SocialShare />
+      </Card.Content>
+    </Card>
+  </Grid.Column>
+);
+
+export default ScorpioGifsUser;
