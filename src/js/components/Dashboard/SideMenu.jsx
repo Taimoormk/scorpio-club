9c04f7cf93133acfe606910bf28aed9e86e202e5
Taimoor Khan <taimoor.m.k@live.com>
2018-04-30 12:54:21 +0500

    SideMenu linting

--- src/js/components/Dashboard/SideMenu.jsx
+++ src/js/components/Dashboard/SideMenu.jsx
@@ -1,37 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Menu, Icon } from 'semantic-ui-react';
 import {
   BrowserRouter as Router,
   Link,
-  Route,
   Switch
 } from 'react-router-dom';
 
-import Dashboard from '../Dashboard';
-import Profile from './Profile';
-import Gifs from './Gifs';
-
-const SideMenu = (props) => {
-  return (
-    <Router>
-      <Switch>
-        <div>
-          <Menu.Item name='home'>
-            <Icon name='home' />
-            <Link to="/dashboard">Dashboard</Link>
-          </Menu.Item>
-          <Menu.Item name='gamepad'>
-            <Icon name='gamepad' />
-            <Link to="/dashboard/profile">Profile</Link>
-          </Menu.Item>
-          <Menu.Item name='camera'>
-            <Icon name='camera' />
-            <Link to="/dashboard/gifs">Gifs</Link>
-          </Menu.Item>
-        </div>
-      </Switch>
-    </Router>
-  );
-};
+const SideMenu = () => (
+  <Router>
+    <Switch>
+      <div>
+        <Menu.Item name='home'>
+          <Icon name='home' />
+          <Link to="/dashboard">Dashboard</Link>
+        </Menu.Item>
+        <Menu.Item name='gamepad'>
+          <Icon name='gamepad' />
+          <Link to="/dashboard/profile">Profile</Link>
+        </Menu.Item>
+        <Menu.Item name='camera'>
+          <Icon name='camera' />
+          <Link to="/dashboard/gifs">Gifs</Link>
+        </Menu.Item>
+      </div>
+    </Switch>
+  </Router>
+);
 
 export default SideMenu;
