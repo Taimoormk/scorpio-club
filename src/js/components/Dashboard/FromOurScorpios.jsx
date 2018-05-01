bc3e5a11ff9b6b22e4295f9a9803bddb9aaa4a89
Taimoor Khan <taimoor.m.k@live.com>
2018-05-01 16:48:44 +0500

    FromOurScorpios page integrated

--- src/js/components/Dashboard/FromOurScorpios.jsx
+++ src/js/components/Dashboard/FromOurScorpios.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Sidebar, Segment, Header, Menu } from 'semantic-ui-react';
 
-const FromOurScorpios = () => (
-  <div>
-    From Our Scorpios
-  </div>
-);
+import Breadcrumbs from './Breadcrumb';
+import SideMenu from './SideMenu';
+
+const FromOurScorpios = (props) => {
+  const { visible, breadcrumbs } = props;
+  return (
+    <div id="from-our-scorpios">
+      <Sidebar.Pushable className="from-our-scorpios-main-content" as={Segment}>
+        <Sidebar as={Menu} animation='overlay' width='thin' visible={visible} icon='labeled' vertical inverted>
+          <SideMenu />
+        </Sidebar>
+        <Sidebar.Pusher>
+          <Segment.Group raised>
+            <Segment>
+              <Breadcrumbs
+                breadcrumbs={breadcrumbs}
+              />
+              <Header as='h3' className="from-our-scorpios-content-heading">From Our Scorpios</Header>
+            </Segment>
+            <Segment textAlign='center'>
+              <Header as='h4' className="from-our-scorpios-content-subheading">Welcome, Name Surname</Header>
+            </Segment>
+          </Segment.Group>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    </div>
+  );
+}
 
 export default FromOurScorpios;
