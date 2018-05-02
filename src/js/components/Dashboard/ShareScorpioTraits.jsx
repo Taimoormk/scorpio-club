0cf64ba7709f87c55c4a06897ebe3155670df9df
Taimoor Khan <taimoor.m.k@live.com>
2018-05-02 10:12:19 +0500

    ShareScorpioTraits integrated

--- src/js/components/Dashboard/ShareScorpioTraits.jsx
+++ src/js/components/Dashboard/ShareScorpioTraits.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Sidebar, Segment, Header, Menu } from 'semantic-ui-react';
 
-const ShareScorpioTraits = () => (
-  <div id="share-scorpio-traits">
-    Share Scorpio Traits
-  </div>
-);
+import Breadcrumbs from './Breadcrumb';
+import SideMenu from './SideMenu';
+
+const ShareScorpioTraits = (props) => {
+  const { visible, breadcrumbs } = props;
+  return (
+    <div id="share-scorpio-traits">
+      <Sidebar.Pushable className="share-scorpio-traits-main-content" as={Segment}>
+        <Sidebar as={Menu} animation='overlay' width='thin' visible={visible} icon='labeled' vertical inverted>
+          <SideMenu />
+        </Sidebar>
+        <Sidebar.Pusher>
+          <Segment.Group raised>
+            <Segment>
+              <Breadcrumbs
+                breadcrumbs={breadcrumbs}
+              />
+              <Header as='h3' className="share-scorpio-traits-content-heading">Share Scorpio Traits</Header>
+            </Segment>
+            <Segment textAlign='center'>
+              <Header as='h4' className="share-scorpio-traits-content-subheading">Welcome, Name Surname</Header>
+            </Segment>
+          </Segment.Group>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    </div>
+  );
+}
 
 export default ShareScorpioTraits;
