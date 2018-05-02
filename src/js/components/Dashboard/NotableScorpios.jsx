12adc7ac1d2afced2789aa382656c88374bfd88b
Taimoor Khan <taimoor.m.k@live.com>
2018-05-02 10:04:25 +0500

    NotableScorpios integrated

--- src/js/components/Dashboard/NotableScorpios.jsx
+++ src/js/components/Dashboard/NotableScorpios.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Sidebar, Segment, Header, Menu } from 'semantic-ui-react';
 
-const NotableScorpios = () => (
-  <div id="notable-scorpios">
-    Notable Scorpios
-  </div>
-);
+import Breadcrumbs from './Breadcrumb';
+import SideMenu from './SideMenu';
+
+const NotableScorpios = (props) => {
+  const { visible, breadcrumbs } = props;
+  return (
+    <div id="notable-scorpios">
+      <Sidebar.Pushable className="notable-scorpios-main-content" as={Segment}>
+        <Sidebar as={Menu} animation='overlay' width='thin' visible={visible} icon='labeled' vertical inverted>
+          <SideMenu />
+        </Sidebar>
+        <Sidebar.Pusher>
+          <Segment.Group raised>
+            <Segment>
+              <Breadcrumbs
+                breadcrumbs={breadcrumbs}
+              />
+              <Header as='h3' className="notable-scorpios-content-heading">Notable Scorpios</Header>
+            </Segment>
+            <Segment textAlign='center'>
+              <Header as='h4' className="notable-scorpios-content-subheading">Welcome, Name Surname</Header>
+            </Segment>
+          </Segment.Group>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    </div>
+  );
+}
 
 export default NotableScorpios;
