f3f941343be53db818cea22b1b992d2617cb6a58
Taimoor Khan <taimoor.m.k@live.com>
2018-05-07 10:39:12 +0500

    minor amends in MarketplaceProducts

--- src/js/components/Dashboard/Marketplace/MarketplaceProducts.jsx
+++ src/js/components/Dashboard/Marketplace/MarketplaceProducts.jsx
@@ -1,5 +1,5 @@
 // ########## Import Dependencies Here ##########
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Grid, Image, Label, Segment, Menu } from 'semantic-ui-react';
 
 // ########## Import Containers Here ##########
@@ -8,46 +8,44 @@ import { Grid, Image, Label, Segment, Menu } from 'semantic-ui-react';
 import Tshirts from './Tshirts';
 import Hoodies from './Hoodies';
 
-class MarketplaceProducts extends Component {
-  render() {
-    const { toggleMarketplaceMenu, switchMarketplaceMenuAction } = this.props;
-    return (
-      <Fragment>
-        <Grid>
-          <Grid.Column width={2}>
-            <Menu fluid vertical tabular>
-              <Menu.Item
-                name='tshirts'
-                active={toggleMarketplaceMenu.activeItem === 'tshirts'}
-                onClick={() => switchMarketplaceMenuAction('thsirts')}
-              />
-              <Menu.Item
-                name='hoodies'
-                active={toggleMarketplaceMenu.activeItem === 'hoodies'}
-                onClick={() => switchMarketplaceMenuAction('hoodies')}
-              />
-              <Menu.Item
-                name='mugs'
-                active={toggleMarketplaceMenu.activeItem === 'mugs'}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name='etc'
-                active={toggleMarketplaceMenu.activeItem === 'etc'}
-                onClick={this.handleItemClick}
-              />
-            </Menu>
-          </Grid.Column>
-          <Grid.Column stretched width={14}>
-            <Segment>
-              {toggleMarketplaceMenu.activeItem === 'tshirts' && <Tshirts />}
-              {toggleMarketplaceMenu.activeItem === 'hoodies' && <Hoodies />}
-            </Segment>
-          </Grid.Column>
-        </Grid>
-      </Fragment>
-    );
-  }
+const MarketplaceProducts = (props) => {
+  const { toggleMarketplaceMenu, switchMarketplaceMenuAction } = props;
+  return (
+    <Fragment>
+      <Grid>
+        <Grid.Column width={2}>
+          <Menu fluid vertical tabular>
+            <Menu.Item
+              name='tshirts'
+              active={toggleMarketplaceMenu.activeItem === 'tshirts'}
+              onClick={() => switchMarketplaceMenuAction('thsirts')}
+            />
+            <Menu.Item
+              name='hoodies'
+              active={toggleMarketplaceMenu.activeItem === 'hoodies'}
+              onClick={() => switchMarketplaceMenuAction('hoodies')}
+            />
+            <Menu.Item
+              name='mugs'
+              active={toggleMarketplaceMenu.activeItem === 'mugs'}
+              onClick={() => switchMarketplaceMenuAction('hoodies')}
+            />
+            <Menu.Item
+              name='etc'
+              active={toggleMarketplaceMenu.activeItem === 'etc'}
+              onClick={() => switchMarketplaceMenuAction('hoodies')}
+            />
+          </Menu>
+        </Grid.Column>
+        <Grid.Column stretched width={14}>
+          <Segment>
+            {toggleMarketplaceMenu.activeItem === 'tshirts' && <Tshirts />}
+            {toggleMarketplaceMenu.activeItem === 'hoodies' && <Hoodies />}
+          </Segment>
+        </Grid.Column>
+      </Grid>
+    </Fragment>
+  );
 }
 
 export default MarketplaceProducts;
